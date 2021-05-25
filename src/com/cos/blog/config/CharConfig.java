d12faa91e2fac55e71de6a8a49248772b91bd3ac
hjin302 <enro302@naver.com>
2021-05-26 00:20:47 +0900

    프로젝트 구조 만들기(1/2)

---
+++ src/com/cos/blog/config/CharConfig.java
@@ -0,0 +1,39 @@
+package com.cos.blog.config;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CharConfig implements Filter{
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
+		
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=utf-8");
+		
+		String username = request.getParameter("username");
+		System.out.println("username : " + username);
+		
+		PrintWriter out = response.getWriter();
+		out.println("hi");
+		out.flush();
+		
+		//chain.doFilter(request, response);
+		
+		
+	}
+	
+	
+}
---
+++ src/com/cos/blog/domain/reply/ReplyDao.java
@@ -0,0 +1,21 @@
+package com.cos.blog.domain.reply;
+
+public class ReplyDao {
+
+	public void save() { // 회원가입
+		
+	}
+
+	public void update() { // 회원수정
+		
+	}
+	
+	public void usernameCheck() { // 아이디 중복 체크
+		
+	}
+	
+	public void findById() { // 회원정보 보기
+		
+	}
+	
+}
